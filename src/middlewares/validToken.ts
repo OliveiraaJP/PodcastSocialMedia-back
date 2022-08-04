062ddddfe77b9520764b56ffbd8b52523c701759
Joao Paulo <oliveirajpufrj@gmail.com>
2022-08-03 23:51:13 -0300

    initial commit

---
+++ src/middlewares/validToken.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+import { chalkLogger } from "../utils/chalkLogger.js";
+
+
+export async function tokenValidator(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+    chalkLogger.logObject('middleware', decoded.jwtId)
+    res.locals.userId = decoded.jwtId
+    next();
+}
+
