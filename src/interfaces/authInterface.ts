e6a2ce0f2c57093e2bfacda97d7da494218875cd
Joao Paulo <oliveirajpufrj@gmail.com>
2022-08-12 15:01:59 -0300

    test

--- src/interfaces/authInterface.ts
+++ src/interfaces/authInterface.ts
@@ -1,4 +1,5 @@
 import { User } from "@prisma/client";
 
+export type UserDataData = Omit<User, 'id'>
 export type UserData = Omit<User, 'id' | 'createdAt' | 'role' >
 export type UserDataSignin = Omit<UserData, 'username' | 'image' >
